5c350232cac02210b8af3c7ee92989d4d98b21d5
Jordan Humphrey <jphumphrey2@gmail.com>
2019-08-03 21:41:01 -0700

    pushy

--- SIMD/SIMD.cpp
+++ SIMD/SIMD.cpp
@@ -1,4 +1,7 @@
 #include "stdafx.h"
+#include <random>
+#include <chrono>
+#include "SIMD.h"
 
 string get_cpu_name()
 {
@@ -61,9 +64,70 @@ void assembler()
 		cout << "sse4.2 is supported" << endl;
 }
 
+void instrinsics()
+{
+	/*int cpuInfo[4];
+	__cpuid(cpuInfo, 1);*/
+
+	auto a = _mm_set_ps(1, 2, 3, 4);
+	auto b = _mm_set_ps(4, 3, 2, 1);
+	auto c = _mm_add_ps(a, b);
+
+	float f = c.m128_f32[0];
+
+	__try {
+		__m256d z = _mm256_set_pd(1, 2, 3, 4);
+	}
+	__except (1)
+	{
+		cout << "Sorry, __m256_set_pd is not allowed" << endl;
+	}
+}
+
+void simple_mad(float* a, float* b, float* c, float* result, const int length)
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		result[i] = a[i] * b[i] + c[i];
+	}
+}
+
+void optimization()
+{
+	using namespace chrono;
+	const int length = 1024 * 1026 * 64;
+	float *a = new float[length];
+	float *b = new float[length];
+	float *c = new float[length];
+	float *result = new float[length];
+
+	mt19937_64 rng(random_device{}());
+	uniform_real_distribution<float> dist(0, 1);
+
+	for (size_t i = 0; i < length; i++)
+	{
+		a[i] = dist(rng);
+		b[i] = dist(rng);
+		c[i] = dist(rng);
+	}
+
+	auto begin = high_resolution_clock::now();
+	simple_mad(a, b, c, result, length);
+	auto end = high_resolution_clock::now();
+	cout << "MAD took up " << duration_cast<milliseconds>(end - begin).count() << "msec" << endl;
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+	delete[] result;
+}
+
 int main(int argc, char* argv[])
 {
+	//assembler();
+	//instrinsics();
 
-	assembler();
+	optimization();
+
 	return 0;
 }
